fbd3190266727aa1a4c76e22438c8d2a48a13ead
RedSpiderMkV <redspidermkv@hotmail.com>
2015-08-13 22:26:45 +0100

    arp methods...

--- src/LANMachines/LanPingerAsync/ArpScanner.cs
+++ src/LANMachines/LanPingerAsync/ArpScanner.cs
@@ -6,6 +6,25 @@ namespace LanDiscovery
 {
     public class ArpScanner
     {
+        #region Public Methods
+
+        public ArpScanner()
+        {
+            // default constructor..
+        } // end method
+
+        public List<string> GetRespondingMachines()
+        {
+            List<string> reachableMachines = new List<string>();
+
+            return reachableMachines;
+        } // end method
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
 
     } // end class
 } // end namespace
