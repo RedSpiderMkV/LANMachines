f99c3cdbfaaf3eaa73e59f54bcd67d204739218b
RedSpiderMkV <redspidermkv@hotmail.com>
2015-09-02 20:33:23 +0100

    update running program code

--- src/LANMachines/LanMachinesRunner/Program.cs
+++ src/LANMachines/LanMachinesRunner/Program.cs
@@ -5,20 +5,26 @@ using LanDiscovery;
 
 namespace LanMachines
 {
-    class Program
+    internal class Program
     {
         internal static void Main(string[] args)
         {
             LanDiscoveryManager lanDiscovery = new LanDiscoveryManager();
             List<string> lanMachines = lanDiscovery.GetNetworkMachines();
 
-            foreach (string lanIp in lanMachines)
-            {
-                Console.WriteLine(lanIp);
-            }
+            printStringList(lanMachines);
 
             Console.WriteLine("LAN ping complete");
             Console.ReadKey();
         } // end method
+
+        private static void printStringList(List<string> stringList)
+        {
+            foreach (string s in stringList)
+            {
+                Console.WriteLine(s);
+            } // end foreach
+        } // end method
+
     } // end class
 } // end namespace
