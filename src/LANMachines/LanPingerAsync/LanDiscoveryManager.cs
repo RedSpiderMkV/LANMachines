49c256204617b6bd8a7fee2559ab2a5373d0f8d7
redspidermkv <redspidermkv@hotmail.com>
2016-01-04 23:03:15 +0000

    IP addresses returned in order via the lan discovery manager

--- src/LANMachines/LanPingerAsync/LanDiscoveryManager.cs
+++ src/LANMachines/LanPingerAsync/LanDiscoveryManager.cs
@@ -30,12 +30,42 @@ namespace LanDiscovery
                 } // end if
             } // end foreach
 
+            activeMachines.Sort(new IPAddressComparer());
+
             return activeMachines;
         } // end method
 
         #endregion
 
         #region Private Methods
+
+        private class IPAddressComparer : IComparer<IPAddress>
+        {
+            public int Compare(IPAddress x, IPAddress y)
+            {
+                byte[] xBytes = x.GetAddressBytes();
+                byte[] yBytes = y.GetAddressBytes();
+
+                if (xBytes.Length != 4 || yBytes.Length != 4)
+                {
+                    return 0;
+                } // end if
+
+                for (int i = 0; i < 4; i++)
+                {
+                    if (xBytes[i] > yBytes[i])
+                    {
+                        return 1;
+                    }
+                    else if (xBytes[i] < yBytes[i])
+                    {
+                        return -1;
+                    } // end if
+                } // end for
+
+                return 0;
+            } // end method
+        } // end class
 
         /// <summary>
         /// Get the list of machines which responded to the lan ping test.
