23229af1ee5ac196ad9dd10e28616b0830084059
RedSpiderMkV <redspidermkv@hotmail.com>
2016-01-25 22:04:44 +0000

    Adding class to hold lan machine info

--- src/LANMachines/LanPingerAsync/LanMachine.cs
+++ src/LANMachines/LanPingerAsync/LanMachine.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Net;
 
 namespace LanDiscovery
 {
     public class LanMachine
     {
+        #region Properties
 
+        public IPAddress MachineIPAddress { get; private set; }
+        public string MachineName { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public LanMachine(IPAddress address, string name)
+        {
+            MachineIPAddress = address;
+            MachineName = name;
+        } // end method
+
+        #endregion
     } // end class
 } // end namespace
